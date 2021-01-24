ee2784c42612ecad329456ec9e9a5b90711f84fc
Pavel Ermakov <pde0177776@gmail.com>
2021-01-24 22:53:40 +0300

    Zadacha_36

---
+++ Zadacha_36/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace Zadacha_36
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxCount = new System.Windows.Forms.TextBox();
+            this.buttonOK = new System.Windows.Forms.Button();
+            this.listBoxGetNum = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(105, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(174, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Количество генерируемых чисел";
+            // 
+            // textBoxCount
+            // 
+            this.textBoxCount.Location = new System.Drawing.Point(146, 48);
+            this.textBoxCount.Name = "textBoxCount";
+            this.textBoxCount.Size = new System.Drawing.Size(100, 20);
+            this.textBoxCount.TabIndex = 1;
+            // 
+            // buttonOK
+            // 
+            this.buttonOK.Location = new System.Drawing.Point(156, 74);
+            this.buttonOK.Name = "buttonOK";
+            this.buttonOK.Size = new System.Drawing.Size(75, 23);
+            this.buttonOK.TabIndex = 2;
+            this.buttonOK.Text = "OK";
+            this.buttonOK.UseVisualStyleBackColor = true;
+            this.buttonOK.Click += new System.EventHandler(this.buttonOK_Click);
+            // 
+            // listBoxGetNum
+            // 
+            this.listBoxGetNum.FormattingEnabled = true;
+            this.listBoxGetNum.Location = new System.Drawing.Point(12, 103);
+            this.listBoxGetNum.Name = "listBoxGetNum";
+            this.listBoxGetNum.Size = new System.Drawing.Size(363, 95);
+            this.listBoxGetNum.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(387, 279);
+            this.Controls.Add(this.listBoxGetNum);
+            this.Controls.Add(this.buttonOK);
+            this.Controls.Add(this.textBoxCount);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxCount;
+        private System.Windows.Forms.Button buttonOK;
+        private System.Windows.Forms.ListBox listBoxGetNum;
+    }
+}
+
---
+++ Zadacha_36/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Zadacha_36
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public static double Precent(int a, int b)
+        {
+            return (b*100)/a;
+        }
+        
+        private void buttonOK_Click(object sender, EventArgs e)
+        {
+            int positiveNumCount = 0;
+            int temprnd;
+            int n = int.Parse(textBoxCount.Text);
+            Random rnd = new Random();
+            StreamWriter sw = new StreamWriter("FILE_36.txt");
+            for (int i = 0; i < n; i++)
+            {
+                temprnd = rnd.Next(-100, 100);
+                sw.WriteLine(temprnd);
+                if (temprnd > 0)
+                    positiveNumCount++;
+            }
+            sw.Close();
+            listBoxGetNum.Items.Add(positiveNumCount);
+            listBoxGetNum.Items.Add("Всего сгенерируемых чисел: " + n + "Процент положительных чисел: " + Precent(n,positiveNumCount));
+        }
+    }
+}
