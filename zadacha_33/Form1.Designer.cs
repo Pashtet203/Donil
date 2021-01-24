b0b688f0d6964637a590ba9a1725ae9ab0e9c8af
Pavel Ermakov <pde0177776@gmail.com>
2021-01-24 20:05:24 +0300

    Zadacha_33

---
+++ zadacha_33/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace zadacha_33
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBoxWriteNum = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonOK = new System.Windows.Forms.Button();
+            this.buttonRead = new System.Windows.Forms.Button();
+            this.textBoxRead = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.buttonClear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxWriteNum
+            // 
+            this.textBoxWriteNum.Location = new System.Drawing.Point(12, 90);
+            this.textBoxWriteNum.Multiline = true;
+            this.textBoxWriteNum.Name = "textBoxWriteNum";
+            this.textBoxWriteNum.Size = new System.Drawing.Size(163, 32);
+            this.textBoxWriteNum.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(9, 64);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(166, 23);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Ввод данных";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // buttonOK
+            // 
+            this.buttonOK.Location = new System.Drawing.Point(12, 128);
+            this.buttonOK.Name = "buttonOK";
+            this.buttonOK.Size = new System.Drawing.Size(75, 23);
+            this.buttonOK.TabIndex = 2;
+            this.buttonOK.Text = "Запись";
+            this.buttonOK.UseVisualStyleBackColor = true;
+            this.buttonOK.Click += new System.EventHandler(this.buttonOK_Click);
+            // 
+            // buttonRead
+            // 
+            this.buttonRead.Location = new System.Drawing.Point(224, 157);
+            this.buttonRead.Name = "buttonRead";
+            this.buttonRead.Size = new System.Drawing.Size(75, 23);
+            this.buttonRead.TabIndex = 3;
+            this.buttonRead.Text = "ОК";
+            this.buttonRead.UseVisualStyleBackColor = true;
+            this.buttonRead.Click += new System.EventHandler(this.buttonRead_Click);
+            // 
+            // textBoxRead
+            // 
+            this.textBoxRead.BackColor = System.Drawing.Color.White;
+            this.textBoxRead.Location = new System.Drawing.Point(224, 90);
+            this.textBoxRead.Multiline = true;
+            this.textBoxRead.Name = "textBoxRead";
+            this.textBoxRead.Size = new System.Drawing.Size(163, 61);
+            this.textBoxRead.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(221, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(166, 23);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Вывод данных";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("buttonClear.BackgroundImage")));
+            this.buttonClear.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.buttonClear.Location = new System.Drawing.Point(150, 128);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(25, 23);
+            this.buttonClear.TabIndex = 6;
+            this.buttonClear.UseVisualStyleBackColor = true;
+            this.buttonClear.Click += new System.EventHandler(this.buttonClear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(457, 536);
+            this.Controls.Add(this.buttonClear);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxRead);
+            this.Controls.Add(this.buttonRead);
+            this.Controls.Add(this.buttonOK);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxWriteNum);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxWriteNum;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonOK;
+        private System.Windows.Forms.Button buttonRead;
+        private System.Windows.Forms.TextBox textBoxRead;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button buttonClear;
+    }
+}
+
---
+++ zadacha_33/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+// Количество и сумма чисел в текстовом файле.
+namespace zadacha_33
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            textBoxRead.ReadOnly = true;
+        }
+        static public int CountNumbers(List<int> list)
+        {
+            int sum = 0;
+            foreach (var e in list)
+            {
+                sum += e;
+            }
+            return sum;
+        }
+        private void buttonOK_Click(object sender, EventArgs e)
+        {
+            string[] arrNumWr = textBoxWriteNum.Text.Split();
+            StreamWriter streamWriter = new StreamWriter("FILE_33.txt");
+            foreach (var temp in arrNumWr)
+            {
+                streamWriter.WriteLine(temp);
+            }
+            streamWriter.Close();
+        }
+
+        private void buttonRead_Click(object sender, EventArgs e)
+        {
+            StreamReader streamReader = new StreamReader("FILE_33.txt");
+            string tempString;
+            List<int> listNum = new List<int>();
+            while ((tempString = streamReader.ReadLine()) != null)
+            {
+                listNum.Add(int.Parse(tempString));
+            }
+            streamReader.Close();
+            textBoxRead.Text = "Количество чисел в файле: " + listNum.Count.ToString() + "\n" + "Сумма чисел: " + CountNumbers(listNum).ToString();
+        }
+
+        private void buttonClear_Click(object sender, EventArgs e)
+        {
+            File.WriteAllText("FILE_33.txt", string.Empty);
+            
+        }
+    }
+}
